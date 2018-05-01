e12a0b0f990b22018a890b95fe9c29bbf698f39b
Sean Oughton <seanoughton@gmail.com>
2018-05-01 00:28:04 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.create(recipe_params)
-    @recipe.ingredients_attributes = params[:recipe][:ingredients_attributes]
+    #@recipe.ingredients_attributes = params[:recipe][:ingredients_attributes]
     redirect_to @recipe
   end
 
